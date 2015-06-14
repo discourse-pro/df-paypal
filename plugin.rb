7f672e9ef1973adb21fa56a7a2133fe449e870b8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-06-14 21:47:14 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,4 @@
-#
+# name: df-paypal
+# about: An auxiliary plugin with common PayPal functionality. Used by «Paid Membership» plugin. Will be used by other plugins with payment functionality.
+# version: 1.0.0
+# authors: Dmitry Fedyuk
