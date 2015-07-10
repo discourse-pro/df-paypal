808352c0140567ba921432c75bf341dd41d8d7f9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-07-10 07:14:44 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,4 @@
 # about: An auxiliary plugin with common PayPal functionality. Used by «Paid Membership» plugin. Will be used by other plugins with payment functionality.
 # version: 1.0.0
 # authors: Dmitry Fedyuk
-gem 'attr_required', '1.0.0'
-gem 'paypal-express', '0.8.1', {require_name: 'paypal'}
 Discourse::Application.config.autoload_paths += Dir["#{Rails.root}/plugins/df-paypal/app/models"]
