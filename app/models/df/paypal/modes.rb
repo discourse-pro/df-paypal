7ff22edccf920fc4d311fe9199c7d3460cc263fd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-08-01 07:06:29 +0300

    no message

---
+++ app/models/df/paypal/modes.rb
@@ -0,0 +1,17 @@
+module ::Df::Paypal
+	require_dependency 'enum_site_setting'
+	class Modes < ::EnumSiteSetting
+		def self.valid_value?(val)
+			val.blank? or values.any? { |v| v[:value] == val.to_s }
+		end
+		def self.values
+			@values ||= [
+				{name: 'Sandbox', value: 'sandbox'},
+				{name: 'Production', value: 'production'},
+			]
+		end
+		def self.translate_names?
+			true
+		end
+	end
+end
